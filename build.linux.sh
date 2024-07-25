82770d9bada6852a07143537b92d06410650ada2
godo <xpbb@qq.com>
2024-07-25 17:10:53 +0800

    add build.sh

---
+++ build.linux.sh
@@ -0,0 +1,4 @@
+wails build -platform linux/amd64 -s -o godoos-linux-amd64
+wails build -platform linux/arm64 -s -o godoos-linux-arm64
+wails build -platform darwin/amd64 -s -o godoos-darwin-amd64
+wails build -platform darwin/arm64 -s -o godoos-darwin-arm64
---
+++ build.win.sh
@@ -0,0 +1,2 @@
+wails build -platform windows/amd64 -nsis -s
+wails build -platform windows/arm64 -nsis -s
