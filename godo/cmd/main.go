c0e480092e3d571adead59f21c0dd503513efc97
刘子旺 <15039612+liu-ziwang123@user.noreply.gitee.com>
2024-10-30 17:39:53 +0800

    fix:修复config获取值断言失败问题

--- godo/cmd/main.go
+++ godo/cmd/main.go
@@ -102,7 +102,7 @@ func OsStart() {
 	fileRouter.HandleFunc("/watch", files.WatchHandler).Methods(http.MethodGet)
 	fileRouter.HandleFunc("/setfilepwd", files.HandleSetFilePwd).Methods(http.MethodGet)
 	fileRouter.HandleFunc("/changefilepwd", files.HandleChangeFilePwd).Methods(http.MethodGet)
-	fileRouter.HandleFunc("/changeisPwd", files.HandleSetIsPwd).Methods(http.MethodGet)
+	fileRouter.HandleFunc("/changeispwd", files.HandleSetIsPwd).Methods(http.MethodGet)
 
 	localchatRouter := router.PathPrefix("/localchat").Subrouter()
 	localchatRouter.HandleFunc("/message", localchat.HandleMessage).Methods(http.MethodPost)
