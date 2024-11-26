62f76e851c5335b895e8104ddc8f86a8d572926a
prr <3099672575@qq.com>
2024-11-26 16:28:51 +0800

    add：密码箱设置

--- frontend/src/stores/filePwd.ts
+++ frontend/src/stores/filePwd.ts
@@ -11,7 +11,7 @@ export const useFilePwdStore = defineStore('filePwd', () => {
     visible: 5
   })
   const pwdList = ref([])
-
+  const hasDefaultPwd = ref(false)
   const addPwd = async (params: any) => {
     await db.addOne('filePwdBox', params)
   }
@@ -22,6 +22,23 @@ export const useFilePwdStore = defineStore('filePwd', () => {
       pwdList.value = await db.getPage('filePwdBox', page.value.current, page.value.size)
     }
     await getPageCount()
+    await checkDefault()
+  }
+  const delPwd = async (id: number) => {
+    await db.delete('filePwdBox', id)
+    //如果之前存在默认密码，则清除
+    // if (params.isDefault == 1) {
+    //   const list = await db.getAll('filePwdBox')
+    //   if (list && list?.length > 0) {
+    //     const item = list[list.length - 1]
+    //     item.isDefault = 1
+    //     await db.update('filePwdBox', item.id, item)
+    //   }
+    // }
+  }
+  const pageChange = async (current: number) => {
+    page.value.current = current
+    getPage()
   }
   const getPageCount = async () => {
     page.value.total = await db.countSearch('filePwdBox')
@@ -32,13 +49,36 @@ export const useFilePwdStore = defineStore('filePwd', () => {
       // 如果有余数，则加1
       page.value.pages++;
     }
-    //console.log(pageCount.value)
     return page.value
+  }
+  // 判断是否有默认密码
+  const checkDefault = async () => {
+    const res = await db.getByField('filePwdBox', 'isDefault', 1)
+    if (res && res.length > 0) {
+      hasDefaultPwd.value = true
+      return
+    } 
+    hasDefaultPwd.value = false
+  }
+  //将其余默认密码设置为空
+  const setDefaultPwd = async () => {
+    const res = await db.getByField('filePwdBox', 'isDefault', 1)
+    if (!res && res.length == 0) {
+      return
+    };
+    for (const item of res) {
+      item.isDefault = 0
+      db.update('filePwdBox', item.id, item)
+    }
   }
   return {
     page,
     pwdList,
+    hasDefaultPwd,
     addPwd,
-    getPage
+    getPage,
+    pageChange,
+    delPwd,
+    setDefaultPwd
   }
 })
