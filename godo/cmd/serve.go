f1b3e7e79028922f060937eabc70dc4c02f3781d
godo <xpbb@qq.com>
2024-12-11 12:57:54 +0800

    change restart error

--- godo/cmd/serve.go
+++ godo/cmd/serve.go
@@ -35,7 +35,7 @@ import (
 )
 
 func OsStop() {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	err := store.StopAllHandler()
 	if err != nil {
